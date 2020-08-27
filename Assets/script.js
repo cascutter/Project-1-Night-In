655ffe393b9eb48a76d39dc69b835be18c1d8e45
Steve Roday <srrodrig.rodriguez@gmail.com>
2020-08-26 19:39:06 -0500

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,18 @@
 // Night in js file
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key": "d0db1deb8cmsh5d63bd8fa1ad8adp19a620jsnb8fb75ceb2ec"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+	
+ 
