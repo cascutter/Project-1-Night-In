224e9c52f0f009054a0a170be0a2cd4a2d231374
Steve Roday <srrodrig.rodriguez@gmail.com>
2020-08-29 13:29:09 -0500

    Added some styling to list results and added second API call. Need to work on second call.

--- Assets/drinkscript.js
+++ Assets/drinkscript.js
@@ -2,7 +2,7 @@
 
 $("#searchBtn").on("click", function(){
 var query = document.getElementById("searchDrinks").value;
-$("#searchDrinks").empty();
+$("#drinksHere").empty();
 var settings = {
 	"async": true,
 	"crossDomain": true,
@@ -15,14 +15,50 @@ var settings = {
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
 
+	var results = response.drinks;
+	console.log(results);
+	for(var i = 0; i < 10; i++) {
 
+		var drinkList = document.getElementById("drinksHere");
+		var listItem = document.createElement("ul");
+		drinkList.append(listItem);
 
+		var drinkItem = document.createElement("li");
+		listItem.append(drinkItem);
+		$(drinkItem).addClass("list")
+		var cocktail = results[i].strDrink;
+		drinkItem.innerHTML = cocktail;
+
+		var infoDiv = document.createElement("div")
+		$(infoDiv).attr("class", "box");
+		listItem.append(infoDiv);
+		var picItem = document.createElement("img");
+		infoDiv.append(picItem);
+		var picUrl = results[i].strDrinkThumb;
+		$(picItem).attr("src", picUrl);
+
+		// testing code for next api call
+		var instr = document.createElement("section");
+		infoDiv.append(instr);
+		instr.innerHTML = ("testing");
+	};
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=11007",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+			"x-rapidapi-key": "d0db1deb8cmsh5d63bd8fa1ad8adp19a620jsnb8fb75ceb2ec"
+		}
+	}
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
 });
-
-
-
 });
 	
  
