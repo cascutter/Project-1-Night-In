2da26084bfdee573edcbe451a1c814c503b51d71
Celine Ross <celineross@gmail.com>
2020-08-29 14:03:41 -0500

    figured out how to pull values from response

--- Assets/recipe.js
+++ Assets/recipe.js
@@ -1,30 +1,31 @@
 //variable for page number in results
 var p = 1;
 //variable for ingredient searching, separated by comma
+var ing = "blueberries"
 
+// $("#ingredientSearchBtn").on("click", function () {
+// 	var ing = //$("#ingredientSearch").value;
+// 		$("#ingredientSearch").empty();
 
-$("#ingredientSearchBtn").on("click", function(){
-	var ing = $("#ingredientSearch").value;
-	$("#ingredientSearch").empty();
-
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://recipe-puppy.p.rapidapi.com/?p=1&i=" + ing,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
-		"x-rapidapi-key": "779922a9bemshc6756065ad9410bp115926jsnfa53da806d8a"
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://recipe-puppy.p.rapidapi.com/?p=1&i=" + ing,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+			"x-rapidapi-key": "779922a9bemshc6756065ad9410bp115926jsnfa53da806d8a"
+		}
 	}
-}
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-	
-	
-});
-})
+	$.ajax(settings).done(function (response) {
+		var parseResponse = (JSON.parse(response));
+		var recipeTitle = parseResponse.results[0].title;
+		console.log(recipeTitle);
+		for (var i = 0; i < 10; i++) {
+			
+		}
 
-//when ingredients are entered, display first 10 recipes that match
-//when keyword is entered, display first 10 recipes that match
-//when both are entered, display first 10 recipes that match
+
+	});
+// })
