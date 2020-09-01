18791709375120779d9eb20550504d5f83482835
Celine Ross <celineross@gmail.com>
2020-08-31 20:26:40 -0500

    button is functional

--- Assets/recipe.js
+++ Assets/recipe.js
@@ -1,7 +1,7 @@
 $("#ingredientSearchBtn").on("click", function () {
-	var query = $("#ingredientSearch").val;
+	var query = $("#ingredientSearch").val();
 	$("#recipeListDiv").empty();
-
+	
 	var settings = {
 		"async": true,
 		"crossDomain": true,
@@ -12,9 +12,33 @@ $("#ingredientSearchBtn").on("click", function () {
 			"x-rapidapi-key": "779922a9bemshc6756065ad9410bp115926jsnfa53da806d8a"
 		}
 	}
-$.ajax(settings).done(function (response) {
-	
-	var parseResponse = (JSON.parse(response));
-	console.log(parseResponse);
-})
-})
+
+	$.ajax(settings).done(function (response) {
+	 var parseResponse = (JSON.parse(response));
+	 console.log(parseResponse.results);
+
+	 for (var i = 0; i < 10; i++) {
+
+	     var recipeListDiv = $("#recipeList");
+	     var recipeList = $("<ul>");
+		 recipeListDiv.append(recipeList);
+		 
+
+	     var recipeItem = $("<li>")
+	     recipeList.append(recipeItem);
+	     $(recipeItem).addClass("list");
+	     var recipeTitle = parseResponse.results[i].title;
+		 recipeItem.text(recipeTitle);
+		 
+		//  var recipeLink = parseResponse.results[i].href;
+		 
+		//  var infoDiv = $("<div>");
+	    //  $(infoDiv).attr("class", "box");
+	    //  recipeList.append(infoDiv);
+	    //  var imgField = $("<img>");
+	    //  infoDiv.append(imgField);
+	    //  var imgURL = parseResponse.results[i].thumbnail;
+	    //  $(imgField).attr("src", imgURL);
+	 }
+});
+});
