3d9067a35e7bb4ffee025a9a2c78007b95c077f7
Steve Roday <srrodrig.rodriguez@gmail.com>
2020-09-02 18:53:11 -0500

    Got second API to call, need to work on layout of called info

--- Assets/drinkscript.js
+++ Assets/drinkscript.js
@@ -2,7 +2,8 @@
 
 $("#searchBtn").on("click", function(){
 var query = document.getElementById("searchDrinks").value;
-$("#drinksHere").empty();
+$("#infoDiv").empty();
+var drinkArray = [];
 var settings = {
 	"async": true,
 	"crossDomain": true,
@@ -18,46 +19,45 @@ $.ajax(settings).done(function (response) {
 
 	var results = response.drinks;
 	console.log(results);
+
 	for(var i = 0; i < 10; i++) {
 
-		var drinkList = document.getElementById("drinksHere");
-		var listItem = document.createElement("ul");
-		drinkList.append(listItem);
+		var drinkItem = document.createElement("h1");
+		$("#infoDiv").append(drinkItem);
+		drinkItem.innerHTML = results[i].strDrink;
 
-		var drinkItem = document.createElement("li");
-		listItem.append(drinkItem);
-		$(drinkItem).addClass("list")
-		var cocktail = results[i].strDrink;
-		drinkItem.innerHTML = cocktail;
-
-		var infoDiv = document.createElement("div")
-		$(infoDiv).attr("class", "box");
-		listItem.append(infoDiv);
 		var picItem = document.createElement("img");
-		infoDiv.append(picItem);
-		var picUrl = results[i].strDrinkThumb;
-		$(picItem).attr("src", picUrl);
+		$("#infoDiv").append(picItem);
+		$(picItem).attr("src", results[i].strDrinkThumb);
 
-		// testing code for next api call
-		var instr = document.createElement("section");
-		infoDiv.append(instr);
-		instr.innerHTML = ("testing");
-	};
-
-	var settings = {
+		
+		var drinkID = (results[i].idDrink);
+		
+		//API call for ingredients and instructions
+		var settings = {
 		"async": true,
 		"crossDomain": true,
-		"url": "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=11007",
+		"url": "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=" + drinkID,
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
 			"x-rapidapi-key": "d0db1deb8cmsh5d63bd8fa1ad8adp19a620jsnb8fb75ceb2ec"
 		}
 	}
+		
+	$.ajax(settings).done(function (respond) {
 	
-	$.ajax(settings).done(function (response) {
-		console.log(response);
+		var resultsd = respond.drinks;
+		console.log(resultsd[0]);
+
+		for(var d = 0; d < resultsd.length ; d++) {
+		var instr = document.createElement("p");
+		//$(instr).addClass("ings");
+		$("#infoDiv").append(instr);
+		instr.innerHTML = resultsd[d].strInstructions;	
+	};
 	});
+	};
 });
 });
 	
