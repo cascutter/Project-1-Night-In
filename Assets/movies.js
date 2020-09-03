0b258e33e4ba0307d3f035205b4ccd2201109c44
Kalena Almand <kalmand1057@gmail.com>
2020-09-02 20:46:02 -0500

    added option elements to html

--- Assets/movies.js
+++ Assets/movies.js
@@ -1,17 +1,22 @@
-//Search Genre
-$("searchButton").on("click", function(){
 
-});
-
-// Api
 var APIKey = "b84f85bbaa5bb55f4e02b13c7fec393a";
-var genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKey + "&language=en-US";
-
-var moviesURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + APIKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=18";
+var genresURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + APIKey + "&language=en-US";
 
 $.ajax({
-	url: genreURL,
-	method: "GET"
-  }).then(function(response) {
-	console.log(response);
-  });
+			url: genresURL,
+			method: "GET"
+		}).then(function(response) {
+			for (var i = 0; i < response.genres.length; i++) {
+				var newEl = $("<option>");
+				newEl.attr("genre-id", response.genres[i].id);
+				newEl.text(response.genres[i].name);
+				$("#genre").append(newEl);
+			}
+			
+		});
+
+	//Search Genre
+	$("#submit-genre").on("click", function() {
+		var option = $("option").attr("genre-id");
+		console.log(option);
+	});
